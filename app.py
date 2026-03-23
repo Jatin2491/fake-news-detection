703d71c54014069eb21dd295d30c0d698db5afdc
Jatin Arora <23052491@kiit.ac.in>
2026-03-23 13:37:21 +0530

    fix deployment error

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 import streamlit as st
 import pickle
 model = pickle.load(open("model.pkl", "rb"))
--- model.py
+++ model.py
@@ -4,27 +4,21 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 import pickle
 
-# Load dataset
 df = pd.read_csv("fake_real_news.csv")
 
 X = df["text"]
 y = df["target"]
 
-# Split
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-# TFIDF
 vectorizer = TfidfVectorizer(stop_words="english", max_df=0.7)
 
 X_train = vectorizer.fit_transform(X_train)
 
-# Model
 model = LogisticRegression(max_iter=1000)
 model.fit(X_train, y_train)
-
-# Save model
 pickle.dump(model, open("model.pkl", "wb"))
 pickle.dump(vectorizer, open("vectorizer.pkl", "wb"))
 
